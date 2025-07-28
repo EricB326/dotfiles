f6d79c00bbf0546e1d381c252a12ff373944af04
Eric <ericbrkic326@gmail.com>
2025-07-28 21:52:22 +1000

    Added new plugin for a status bar.

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+	'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = {
+		template = 'gruvbox_dark',
+		sections = {
+			lualine_a = {'mode'},
+			lualine_b = {'branch'},
+			lualine_c = {'filename'},
+			lualine_x = {'filetype'},
+			lualine_y = {''},
+			lualine_z = {''},
+		},
+	}
+}
