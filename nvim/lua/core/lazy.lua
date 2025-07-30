1011b2ad90de47ab9e1e14e08c9a875e774cb19f
Eric <ericbrkic326@gmail.com>
2025-07-30 19:05:12 +1000

    Formatted file.

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -16,13 +16,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+	spec = {
+		{ import = "plugins" },
+	},
+	install = { colorscheme = { "habamax" } },
+	checker = { enabled = true },
 
-  rocks = {
-    enabled = false,
-  }
+	rocks = {
+		enabled = false,
+	}
 })
