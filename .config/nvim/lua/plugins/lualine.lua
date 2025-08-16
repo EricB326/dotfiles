3ca28f2a91d4e529cdcd1354b96270f5ec26d135
Eric Brkic (@Archtop) <ericbrkic326@gmail.com>
2025-08-16 12:49:09 +1000

    [NVIM - Lualine] Added progress % to lualine plugin config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,15 +1,15 @@
 return {
 	'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
 		template = 'gruvbox_dark',
 		sections = {
-			lualine_a = {'mode'},
-			lualine_b = {'branch'},
-			lualine_c = {'filename'},
-			lualine_x = {''},
-			lualine_y = {''},
-			lualine_z = {'filetype'},
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch' },
+			lualine_c = { 'filename' },
+			lualine_x = { '' },
+			lualine_y = { 'progress' },
+			lualine_z = { 'filetype', },
 		},
 	}
 }
